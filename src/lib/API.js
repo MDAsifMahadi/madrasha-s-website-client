db70fc5bcf6ce01cc31fbed2b145b487c714bb7d
MDAsifMahadi <mahadiasif3@gmail.com>
2025-02-13 23:25:31 +0600

    add button componnent

--- src/lib/API.js
+++ src/lib/API.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const API = axios.create({
     baseURL : "https://madrasha-s-website-server.onrender.com",
-    // baseURL : "http://localhost:4000",
 })
 
 export default API;
